26f096854f5905214868e25f455e0a6497f3dfa3
lc451574367 <39795448+lc451574367@users.noreply.github.com>
2021-11-17 21:03:26 +0800

    Update distance.py

--- sempsy-1.0/semdistance/distance.py
+++ sempsy-1.0/semdistance/distance.py
@@ -3,10 +3,12 @@ import gensim
 import numpy as np
 from textprocess.textcut import *
 
-#词向量模型
+# 词向量模型
+# word vector model
 model = gensim.models.KeyedVectors.load_word2vec_format(os.path.join('model_baiduwiki_sg_300d.bin'), binary=True)
 
 # 获取词语向量
+# get word vector
 def get_vector(word):
     try:
         return model[word]
@@ -16,16 +18,19 @@ def get_vector(word):
         # return np.zeros(300)[: int(300)]
 
 # norm计算向量的模；求单位向量
+# unit vector calculation
 def _unitvec(v): return v/ np.linalg.norm(v)
 
 
 # 求词语之间的相似性，即求向量的余弦值
+# similarities calculation between words
 def similarity(word1, word2, size=300):
     return np.dot(_unitvec(get_vector(word1)),
                   _unitvec(get_vector(word2)))
 
 
-# #求句子平均向量
+# 求句子平均向量
+# get sentence vector by averaging word vector in the sentence
 def mean_vector_sentence(s):
     vectorized_sentence = [get_vector(w) for w in textcut(s)]
     mean_vector_sentence = np.mean(vectorized_sentence, 0)
@@ -33,10 +38,11 @@ def mean_vector_sentence(s):
 
 
 # 求句子相似性
+# similarities calculation between sentences
 def sim_sentence(s1, s2):
     return np.dot(_unitvec(mean_vector_sentence(s1)), _unitvec(mean_vector_sentence(s2)))
 
-
+# 
 def mean_distances(vectors, vec_len):
     dists = []
     for i in range(vec_len):
@@ -48,6 +54,7 @@ def mean_distances(vectors, vec_len):
 
 
 # 基于词语的文章平均语义距离
+# word-based text mean semantic distance calculation
 def text_distance_word(text):
     words = textcut(text)
     wc = len(words)  # 词语个数
@@ -59,6 +66,7 @@ def text_distance_word(text):
 
 
 # 基于句子的文章平均语义距离,text为文本，wind为句子长度,句子间不重叠
+# sentence-based text mean semantic distance calculation(sentence no overlap)
 def text_distance_sentence(text, wind):
     wcut = textcut(text)  # 分词
     # k = 5 #i的步长
@@ -78,6 +86,7 @@ def text_distance_sentence(text, wind):
 
 
 # 计算全局语义距离，即文章的平均语义距离，滑动窗口，句子间有重叠，k为滑动距离
+# sentence-based text mean semantic distance calculation(sentence overlap)
 def distance_window(text, wind, k):
     words_cut = textcut(text)  # 分词
     wc = len(words_cut)
@@ -105,5 +114,6 @@ def distance_window(text, wind, k):
 
 
 # 求两文本相似性(以词语为单位)
+# similarities calculation between texts
 def sim_text(text1, text2):
     return np.dot(text_distance_word(text1)[1], text_distance_word(text2)[1])
