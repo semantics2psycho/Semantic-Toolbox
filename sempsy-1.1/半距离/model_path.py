c8a9dd197ecb909d10b76d54e00654b51133e2b9
lc451574367 <39795448+lc451574367@users.noreply.github.com>
2022-06-07 21:08:13 +0800

    Delete model_path.py

--- sempsy-1.1/半距离/model_path.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os
-
-model_test_path = os.path.join('test\\word_vector_win10_300d.bin')
-
-model_trained_path = "H:\\Corpus\\corpus-nostop-cut\\baidu_wiki_corpus_300_new_binary.bin"
